f12423e7af4f1bcc7c47f30e7440fa4ee5c8b936
sebastiangs23 <sebastiangs2309@gmail.com>
2026-06-15 19:26:35 +0400

    fix: hot fix in the sizing

--- modules/home/presentation/components/HomePage.tsx
+++ modules/home/presentation/components/HomePage.tsx
@@ -4,10 +4,10 @@ import { HomeHero } from "./HomeHero";
 
 export function HomePage() {
   return (
-    <>
+    <div className="mx-auto max-w-[1440px] px-6 lg:px-10">
       <TopOfferBar />
       <Navbar />
       <HomeHero />
-    </>
+    </div>
   );
 }
